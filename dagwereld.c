a87f9f63d3ba64c3672fdab0e6e7a94a03e28c45
bvankeulen <bert.vankeulen@jjay.cuny.edu>
2014-06-17 10:55:51 -0400

    Update dagwereld.c

--- dagwereld.c
+++ dagwereld.c
@@ -2,9 +2,5 @@
 // add some comments
 int main(int argc, char* argv[])
 {
-<<<<<<< HEAD
-    printf("Dag Wereld, local update");
-=======
     printf("Dag Wereld public update");
->>>>>>> origin/master
 }
